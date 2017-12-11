8ca706da75a5e011f394b22360c9be285f6425f3
Erle Carrara <carrara.erle@gmail.com>
2017-12-11 17:12:55 -0200

    v0.0.2

--- src/backends/dummy.js
+++ src/backends/dummy.js
@@ -1,15 +1,23 @@
 export default (options, config) => {
-  const { progress } = options
+  const { progress, fileInput } = options
   return new Promise((resolve, reject) => {
-    let dummyProgress = 0.0
-    const intervalId = window.setInterval(() => {
-      progress(dummyProgress)
-      dummyProgress += 0.1
+    if (fileInput.files.length > 0) {
+      let dummyProgress = 0.0
+      const intervalId = window.setInterval(() => {
+        progress(dummyProgress)
+        dummyProgress += 0.1
 
-      if (dummyProgress >= 1.0) {
-        window.clearInterval(intervalId)
-        resolve()
-      }
-    }, 100)
+        if (dummyProgress >= 1.0) {
+          window.clearInterval(intervalId)
+          resolve({
+            path: `s3://example/${fileInput.files[0].name}`
+          })
+        }
+      }, 100)
+    } else {
+      resolve({
+        path: null
+      })
+    }
   })
 }
