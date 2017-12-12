1f9957ad57ca54d3714f1ee3d424ba1239f906fc
Erle Carrara <carrara.erle@gmail.com>
2017-12-12 14:22:57 -0200

    v0.0.4

---
+++ index.js
@@ -0,0 +1,9 @@
+import dummyBackend from './src/backends/dummy'
+import evaporateBackend from './src/backends/evaporate'
+import UploadFileField from './src/components/UploadFileField'
+
+export default {
+  UploadFileField
+  dummyBackend,
+  evaporateBackend
+}
